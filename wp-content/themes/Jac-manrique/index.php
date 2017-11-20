60bb073aa7b8da8198c5700c7548c35045071c56
Guiyolond <31778894+Guiyolond@users.noreply.github.com>
2017-11-20 17:38:16 -0500

    Trying a layout
    Jugando con la rejilla de bootstrap

--- wp-content/themes/Jac-manrique/index.php
+++ wp-content/themes/Jac-manrique/index.php
@@ -2,12 +2,20 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <!--js bootstrap-->
+    <!--css bootstrap-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <title>Jac Manrique Central</title>
   </head>
   <body>
-    <h1>hola</h1>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-3"></div>
+        <div class="col-3"></div>
+        <div class="col-3"></div>
+      </div>
+
+
+    </div>
 
 
     <!--js bootstrap-->
