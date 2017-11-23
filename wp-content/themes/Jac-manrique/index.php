4f91d906cb1b55eadcf7f34d8affe548b50c7314
a-serna <alejo.tanatos@gmail.com>
2017-11-22 19:45:27 -0500

    add logo jac

--- wp-content/themes/Jac-manrique/index.php
+++ wp-content/themes/Jac-manrique/index.php
@@ -4,8 +4,7 @@
     <header>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">
-          <!-- <img src="/img/logo-manrique.png"> -->
-          logo
+          <img src="wp-content/themes/Jac-manrique/img/logo-manrique.png" class="img-fluid float-left" alt="Responsive image">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -36,27 +35,20 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-sm">
-          columna 01
+          columna
         </div>
         <div class="col-sm">
-          columna 02
+          columna
         </div>
         <div class="col-sm">
-          columna 03
+          columna
         </div>
         <div class="col-sm">
-          <h1>vf</h1>columna 04
-          <h2>eds</h2>
+          columna
         </div>
       </div>
     </div>
 
     <i class="material-icons">code</i>
 
-
-    <!--js bootstrap-->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-  </body>
-</html>
+    <?php get_footer(); ?>
