5be06b8d5751f4ea2a5ee7730c441e76ef00de78
a-serna <alejo.tanatos@gmail.com>
2017-11-23 10:13:39 -0500

    add cards mobility
    agregue las tarjetas con la info de movilidad de la comuna

--- wp-content/themes/Jac-manrique/index.php
+++ wp-content/themes/Jac-manrique/index.php
@@ -13,19 +13,19 @@
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
           <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
             <li class="nav-item">
-              <a class="nav-link" href="#nosotros">Nosotros</a>
+              <a class="nav-link" href="#nosotros"><p>Nosotros</p></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#comuna">Comuna</a>
+              <a class="nav-link" href="#comuna"><p>Mi comuna</p></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#maps">Mapas</a>
+              <a class="nav-link" href="#maps"><p>Mapas</p></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#blog">Blog</a>
+              <a class="nav-link" href="#blog"><p>Blog</p></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#contacto">Contacto</a>
+              <a class="nav-link" href="#contacto"><p>Contacto</p></a>
             </li>
           </ul>
         </div>
@@ -82,16 +82,16 @@
     <section id="comuna" class="container">
       <div class="row">
         <div class="col-sm">
-          <h2 class="text-green">Comuna</h2>
+          <h2 class="text-green">Mi comuna</h2>
           <p>La comuna 4 tiene una interesante geografía con una larga trayectoria historica y cultural.</p>
         </div>
       </div>
       <div class="row media">
-        <div class="col-sm-12 col-lg-5">
+        <div class="col-sm-12 col-lg-4">
         <!-- imagen aqui -->
         <img class="mr-3" src="..." alt="Generic placeholder image">
         </div>
-        <div class="col-sm-12 col-lg-7">
+        <div class="col-sm-12 col-lg-8">
           <div class="media-body">
             <h2 class="mt-0 text-green">Comuna 4</h2>
             <p><b class="text-green">Construcción:</b> su inicio fue en el año de 1.920 con la construcción del tranvía eléctrico
@@ -109,6 +109,98 @@
             <p><b class="text-green">Estratificación:</b> los estratos de la comuna 4 Aranjuez son el 1, 2 y 3.</p>
             <p><b class="text-green">Viviendas:</b> en nuestro territorio se encuentran 45.000 viviendas entre casas,
               apartamentos y locales comerciales.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm">
+          aqui insertamos un video
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm">
+          <h2 class="text-green">Movilidad</h2>
+          <p>La comuna 4 cuenta con una alta variedad de rutas y medios de transporte para el servicio
+            público de transporte, que atienden su población.</p>
+        </div>
+      </div>
+
+      <div class="row media">
+
+        <div class="my-border border border-success rounded col-sm-12 col-lg-5 p-4 mb-3">
+          <div class="row">
+            <div class="col-sm-3">
+              <img class="mr-3" src="..." alt="Generic placeholder image">
+            </div>
+            <div class="col-sm-9">
+              <div class="media-body">
+                <p>Las empresas de buses que recorren el territorio son: Aranjuez, Campo Valdés, Santa Cruz.
+                  Combuses, Zamora, Bello y Coopetransa.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="my-border border border-success rounded col-sm-12 col-lg-5 offset-lg-2 p-4 mb-3">
+          <div class="row">
+            <div class="col-sm-3">
+              <img class="mr-3" src="..." alt="Generic placeholder image">
+            </div>
+            <div class="col-sm-9">
+              <div class="media-body">
+                <p>El servicio de taxis que circulan en nuestras vías es muy eficiente.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="my-border border border-success rounded col-sm-12 col-lg-5 p-4 mb-3">
+          <div class="row">
+            <div class="col-sm-3">
+              <img class="mr-3" src="..." alt="Generic placeholder image">
+            </div>
+            <div class="col-sm-9">
+              <div class="media-body">
+                <p>El Metro de Medellín tiene cuatro estaciones en la comuna 4 Aranjuez que son: Estación
+                  Hospital, Estación Universidad de Antioquia, Estación Terminal de Transporte y la Estación
+                  Tricentenario.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="my-border border border-success rounded col-sm-12 col-lg-5 offset-lg-2 p-4 mb-3">
+          <div class="row">
+            <div class="col-sm-3">
+              <img class="mr-3" src="..." alt="Generic placeholder image">
+            </div>
+            <div class="col-sm-9">
+              <div class="media-body">
+                <p>El Metro Plus tiene siete estaciones que son: Estación Aranjuez, Estación Berlín, Estación
+                  las Esmeraldas, Estación Manrique, Estación Gardel, Estación Palos Verdes y Estación
+                  Hospital.
+                  Es de resaltar que el Metro y el Metro Plus están articulados con el sistema de cables y el
+                  tranvía de la ciudad.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="my-border border border-success rounded col-sm-12 col-lg-12 p-4 mb-3">
+          <div class="row">
+            <div class="col-sm-3">
+              <img class="mr-3" src="..." alt="Generic placeholder image">
+            </div>
+            <div class="col-sm-9">
+              <div class="media-body">
+                <p>Gran cantidad de habitantes de la comuna tienen vehículo particular o moto para su
+                  movilidad.
+                  La Movilidad en la comuna 4 Aranjuez es muy eficiente.</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
