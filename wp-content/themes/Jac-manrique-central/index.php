2c9f00ea3bb0ada3f96742ac36c9464bd782dbd0
a-serna <alejo.tanatos@gmail.com>
2017-12-12 00:21:36 -0500

    material icon in css
    agregue las fuentes de mateial icon design en el css hospedadas en la carpeta font

--- wp-content/themes/Jac-manrique-central/index.php
+++ wp-content/themes/Jac-manrique-central/index.php
@@ -22,11 +22,11 @@
           </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="carousel-control-prev-icon" aria-hidden="true"><i class="material-icons">email</i></span>
           <span class="sr-only">Previous</span>
         </a>
         <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="carousel-control-next-icon" aria-hidden="true"><i class="material-icons">email</i></span>
           <span class="sr-only">Next</span>
         </a>
       </div>
