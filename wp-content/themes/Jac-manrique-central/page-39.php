b90c738ae62ac745fb1076c5816a69321df66c3d
a-serna <alejo.tanatos@gmail.com>
2017-12-12 19:12:02 -0500

    arrow slide comuna

--- wp-content/themes/Jac-manrique-central/page-39.php
+++ wp-content/themes/Jac-manrique-central/page-39.php
@@ -1,39 +1,6 @@
 <?php get_header("blog") ?>
 
 <article id="home">
-    <div id="carouselExampleIndicators" class="carousel slide animated bounceInLeft" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img class="d-block w-100" src="<?php bloginfo('template_url'); ?>/img/logo-manrique.png" alt="1rst slide">
-        </div>
-        <div class="carousel-item">
-          <img class="d-block w-100" src="<?php bloginfo('template_url'); ?>/img/logo-manrique.png" alt="2nd slide">
-        </div>
-        <div class="carousel-item">
-          <img class="d-block w-100" src="<?php bloginfo('template_url'); ?>/img/logo-manrique.png" alt="3Th slide">
-        </div>
-        <div class="carousel-item">
-          <img class="d-block w-100" src="<?php bloginfo('template_url'); ?>/img/logo-manrique.png" alt="4th slide">
-        </div>
-      </div>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
-</article>
-
-<section>
   <div class="row pt-5 pl-5 pr-5 d-flex align-self-start">
          <div class="col-lg-3 col-md-6 col-xs-12 pb-3">
              <?php get_sidebar(); ?>
@@ -75,6 +42,10 @@
         ?>
        <!-- fin del loop -->
      </div>
+</article>
+
+<section>
+  <p>colocar la paginación del blog</p>
 </section>
 
 
